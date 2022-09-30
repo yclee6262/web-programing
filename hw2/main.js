30dd50da9946094aeb4126c521201ab5161e5eb4
yclee6262 <yclee6262@gmail.com>
2022-09-30 20:11:14 +0800

    add host to guest block

--- hw2/main.js
+++ hw2/main.js
@@ -14,4 +14,20 @@ function full_screen(){
     document.getElementById("host_hover").style.left = "700px";
 }
 
+let host = document.getElementById("host");
+let guest = document.getElementById("all_guests");
 
+function unpin(item){
+    document.getElementById("sec_host").style.display = "none";
+    document.getElementById("sec_guest").style.flexBasis = "100%";
+    document.getElementById("sec_guest").style.height = "38em";
+    document.getElementById("odd_guest").style.width = "auto";
+    document.getElementById("footer").style.top = "38em"
+    host.parentNode.removeChild(host);
+    guest.appendChild(host);
+    host.style.background = "rgb(34, 40, 46)";
+    host.style.padding = "0.5px 6px 5px";
+    host.style.justifyContent = "center";
+    host.style.position = "relative";
+    host.style.borderRadius = "5%";
+}
