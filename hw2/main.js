b821da3773b8dc97546fc6310f5fd565cbcdee66
yclee6262 <yclee6262@gmail.com>
2022-09-30 14:48:44 +0800

    can remove guests and have full screen host

--- hw2/main.js
+++ hw2/main.js
@@ -1,1 +1,17 @@
-hello
+function remove_guest(item){
+    item.parentNode.remove();
+    var guests = document.getElementById("all_guests")
+    console.log(guests.childElementCount);
+    if (guests.childElementCount === 0) {
+        full_screen();
+    }
+}
+
+function full_screen(){
+    document.getElementById("sec_guest").style.display = "none";
+    document.getElementById("sec_host").style.flexBasis = "100%";
+    document.getElementById("host_hover").style.top = "380px";
+    document.getElementById("host_hover").style.left = "700px";
+}
+
+
