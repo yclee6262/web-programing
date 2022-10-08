f724885995d6a205637ba61377cc9c80ad52dcc9
yclee6262 <yclee6262@gmail.com>
2022-10-09 01:48:42 +0800

    Update index.js

--- hw3/src/pages/Home/index.js
+++ hw3/src/pages/Home/index.js
@@ -2,6 +2,7 @@ import Header from "./components/header";
 import Main from "./components/main";
 import Footer from "./components/footer";
 import "./styles.css";
+import { useEffect } from "react";
 
 const Home = () => {
   return (
