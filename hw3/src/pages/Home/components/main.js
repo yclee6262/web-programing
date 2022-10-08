1d94d05b54523b0cf48e58a6f9d6e2827fd19455
yclee6262 <yclee6262@gmail.com>
2022-10-08 21:00:09 +0800

    can add items

--- hw3/src/pages/Home/components/main.js
+++ hw3/src/pages/Home/components/main.js
@@ -1,12 +1,33 @@
 import Item from './item';
+import { useState } from "react"
 
-const Main = ({listdata}) => {
+const Main = () => {
 
+    const [data, setdata] = useState([]);
+    const [note, setnote] = useState('')
+    function addItem() {
+        setdata(function(prev){
+            return [...prev, note]
+        })
+    }
+
+    function noteChange(e) {
+        setnote(e.target.value)
+    }
+
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            console.log("yes")
+            addItem();
+        }
+    }
+    
     return <div className='todo-app__main'>
-        <input type="text" className="todo-app__input" placeholder="What need to be done?"></input>
+        <input type="text" className="todo-app__input" placeholder="What need to be done?"  onKeyDown={handleKeyDown} 
+        value={note} onChange={noteChange}></input>
         <ul className="todo-app__list" id="todo-list">
             {
-                listdata.map(item => <Item />)
+                data.map(item => <Item/>)
             }
         </ul>
     </div>
