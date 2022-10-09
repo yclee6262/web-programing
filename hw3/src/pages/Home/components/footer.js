0c05b11d6947335584d217b793c81e6792caafa8
yclee6262 <yclee6262@gmail.com>
2022-10-09 17:48:05 +0800

    Update footer.js

--- hw3/src/pages/Home/components/footer.js
+++ hw3/src/pages/Home/components/footer.js
@@ -3,9 +3,13 @@ const Footer = ({TODO_num}) => {
     <div className="todo-app__footer" id="todo-footer">
       <div className="todo-app__total">{TODO_num + 1} left</div>
       <ul className="todo-app__view-buttons">
-        <li>all</li>
+        <button>All</button>
+        <button>Active</button>
+        <button>Completed</button>
       </ul>
-      <div className="todo-app__clean">clear</div>
+      <div className="todo-app__clean">
+        <button>clear</button>
+      </div>
     </div>
   );
 };
