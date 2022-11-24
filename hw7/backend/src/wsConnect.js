31f524ef59e832293ae54775373aedc3a880fe91
yclee6262 <yclee6262@gmail.com>
2022-11-24 10:13:59 +0800

    Update wsConnect.js

--- hw7/backend/src/wsConnect.js
+++ hw7/backend/src/wsConnect.js
@@ -1,9 +1,15 @@
 import Message from "../models/message"
 
 const sendData = (data, ws) => {
-    ws.send(JSON.stringify(data)); }
+    ws.send(JSON.stringify(data)); };
 const sendStatus = (payload, ws) => {
-    sendData(["status", payload], ws); }
+    sendData(["status", payload], ws); };
+// const broadcastMessage = (wss, data, status) => {
+//     wss.clients.forEach((client) => {
+//         sendData(data, client);
+//         sendStatus(status, client);
+//     });
+// };
 export default {
     initData: (ws) => {
         Message.find().sort({ created_at: -1 }).limit(100).exec((err, res) => {
@@ -24,16 +30,29 @@ export default {
                     const message = new Message({name, body})
                     try { await message.save();
                     } catch(e) {throw new Error ("Message DB save error: " + e);}
+                    // broadcastMessage(
+                    //     ws,
+                    //     ['output', [payload]],
+                    //     {
+                    //         type: 'success',
+                    //         msg: 'Message sent.'
+                    //     });
+                        
                     // Respond to client
                     sendData(['output', [payload]], ws)
                     sendStatus({
                         type: 'success',
                         msg: 'Message sent.'
                     }, ws)
-                    break
+                    break;
                 }
                 case 'clear': {
                     Message.deleteMany({}, () => {
+                    //     broadcastMessage(
+                    //         ws, 
+                    //         ['cleared'],
+                    //         { type: 'info', msg: 'Message cache cleared.'})
+                    // })
                     sendData(['cleared'], ws)
                     sendStatus
                     ({ type: 'info', msg: 'Message cache cleared.'}, ws)
